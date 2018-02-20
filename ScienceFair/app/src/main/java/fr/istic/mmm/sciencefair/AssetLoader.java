38a8e062c12dd45dda55bd93b0ee47c4f1c235ea
PierreGaudichon <pierre@gaudichon.com>
2018-02-20 16:03:06 +0100

    Make AssetLoader.

---
+++ ScienceFair/app/src/main/java/fr/istic/mmm/sciencefair/AssetLoader.java
@@ -0,0 +1,45 @@
+package fr.istic.mmm.sciencefair;
+
+import android.content.res.AssetManager;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+
+import fr.istic.mmm.sciencefair.data.Data;
+
+public class AssetLoader {
+
+    public static final String SMALL = "fr-esr-fete-de-la-science-17.small.json";
+
+    private AssetManager assets;
+    private Gson gson;
+
+    public AssetLoader(AssetManager assets) {
+        this.assets = assets;
+        this.gson = new Gson();
+    }
+
+    private String loadString(String path) {
+        String json = null;
+        try {
+            InputStream stream = assets.open(path);
+            int size = stream.available();
+            byte[] buffer = new byte[size];
+            stream.read(buffer);
+            stream.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return json;
+    }
+
+    public List<Data> loadData(String path) {
+        return Arrays.asList(gson.fromJson(loadString(path), Data[].class));
+    }
+
+}
--- ScienceFair/app/src/main/java/fr/istic/mmm/sciencefair/EventList.java
+++ ScienceFair/app/src/main/java/fr/istic/mmm/sciencefair/EventList.java
@@ -1,6 +1,7 @@
 package fr.istic.mmm.sciencefair;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -23,33 +24,18 @@ import fr.istic.mmm.sciencefair.data.Data;
 import fr.istic.mmm.sciencefair.data.DataListAdapter;
 
 public class EventList extends AppCompatActivity {
-
-    public String loadJSONFromAsset() {
-        String json = null;
-        try {
-            InputStream stream = getAssets().open("fr-esr-fete-de-la-science-17.small.json");
-            int size = stream.available();
-            byte[] buffer = new byte[size];
-            stream.read(buffer);
-            stream.close();
-            json = new String(buffer, "UTF-8");
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            return null;
-        }
-        return json;
-    }
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_list);
 
+        AssetLoader loader = new AssetLoader(getAssets());
+
         ListView list = (ListView) findViewById(R.id.list_main);
         View item = findViewById(R.id.item_layout);
 
-        Gson gson = new Gson();
-        List<Data> test = Arrays.asList(gson.fromJson(loadJSONFromAsset(), Data[].class));
+        List<Data> test = loader.loadData(AssetLoader.SMALL);
         ListAdapter adapter = new DataListAdapter(this, test);
         list.setAdapter(adapter);
     }
