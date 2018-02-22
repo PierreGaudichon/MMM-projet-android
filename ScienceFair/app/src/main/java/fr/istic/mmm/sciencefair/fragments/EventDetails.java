903c1c56fb9cc7a2e6cfdb11c8262c4450db8f79
PierreGaudichon <pierre@gaudichon.com>
2018-02-22 10:01:35 +0100

    Revert Jules mistake.

--- ScienceFair/app/src/main/java/fr/istic/mmm/sciencefair/fragments/EventDetails.java
+++ ScienceFair/app/src/main/java/fr/istic/mmm/sciencefair/fragments/EventDetails.java
@@ -39,12 +39,15 @@ public class EventDetails extends Fragment  { /*implements View.OnClickListener*
         ((TextView) view.findViewById(R.id.event_description)).setText(event.fields.description_longue_fr);
     }
 
-    private void share() {
-        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
-        sharingIntent.setType("text/plain");
-        String shareBody = "ScienceFair : " + event.fields.titre_fr + "\n" + event.fields.lien;
-        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
-        startActivity(Intent.createChooser(sharingIntent, "Share using"));
+    public void share() {
+        String name = getResources().getString(R.string.app_name);
+        String subject = name + " : " + event.fields.titre_fr;
+        String body = subject + "\n" + event.fields.lien;
+        Intent intent = new Intent(android.content.Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(android.content.Intent.EXTRA_TEXT, body);
+        startActivity(Intent.createChooser(intent, "Share using"));
     }
     /*//Firebase
     public void logEvent(View view){
