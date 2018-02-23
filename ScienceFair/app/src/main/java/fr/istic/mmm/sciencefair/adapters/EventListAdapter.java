e755957353dd812cc2b0546afdcabd30c6e6f1d9
MAEL <mael.petit@wanadoo.fr>
2018-02-23 18:25:03 +0100

    colored title according to remaining places

--- ScienceFair/app/src/main/java/fr/istic/mmm/sciencefair/adapters/EventListAdapter.java
+++ ScienceFair/app/src/main/java/fr/istic/mmm/sciencefair/adapters/EventListAdapter.java
@@ -1,6 +1,7 @@
 package fr.istic.mmm.sciencefair.adapters;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,6 +23,13 @@ public class EventListAdapter extends ListAdapter<Event> {
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         Event event = list.get(position);
+        int color = Color.rgb(115, 224, 65);
+        if ((event.eventFirebase != null && event.eventFirebase.remaining == -1) || event.eventFirebase == null){
+            color = Color.rgb(255, 142, 43);
+        }else if(event.eventFirebase.remaining < 5) {
+            color = Color.rgb(226, 49, 22);
+        }
+        ((TextView) holder.view.findViewById(R.id.titre)).setTextColor(color);
         ((TextView) holder.view.findViewById(R.id.titre)).setText(event.fields.titre_fr);
         ((TextView) holder.view.findViewById(R.id.description)).setText(event.fields.description_fr);
         holder.view.setOnClickListener(l -> {
