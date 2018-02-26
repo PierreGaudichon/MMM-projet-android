0e0590d4ec75f0ee5fa8f1317ab1c8b4e2367cfa
MAEL <mael.petit@wanadoo.fr>
2018-02-26 16:36:13 +0100

    Small fix

--- ScienceFair/app/src/main/java/fr/istic/mmm/sciencefair/MainActivity.java
+++ ScienceFair/app/src/main/java/fr/istic/mmm/sciencefair/MainActivity.java
@@ -145,7 +145,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void showCourseList() {
-        isSharable = true;
+        isSharable = false;
         showFullFragment(courseList);
     }
 
